2d287307802f4721fdcfcb070a13ad31d991b06b
Nathan Serna <nath2534@outlook.com>
2024-10-20 11:47:32 -0500

    Displaying answers

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -3,19 +3,39 @@
 import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI';
 import { useRoute } from 'vue-router';
-
+import BaseTitle from '@/components/BaseTitle.vue';
 
 
 const api = useAPI()
 const question = ref(null)
 const route = useRoute()
-
+const answer = ref([])
 
 
 onMounted(async () => {
   question.value = await api.getQuestion(route.params.id)
 
+
+ answers.value.push({
+    id: answers.value.length, 
+    correct: true, 
+    answer: question.value.correct_answer
+  })
+  question.value.incorrect_answers.map((wrong_answer)=> {
+    answers.value.push({
+      id: answers.value.length, 
+      correct: false, 
+      answer: wrong_answer
+    })
+  })
+
+ 
 })
+
+
+
+
+
 
 
 
@@ -28,13 +48,18 @@ onMounted(async () => {
 
 
 <template>
- 
-{{ question }}
+
+  <div v-if="question" class="">
+    <BaseTitle>{{ question.category }} </BaseTitle>
+    {{ question.question }}
+
+<div v-for="answer in answers" v-html="answer.answer" :key="answer.id"></div>
+
+</div>
 
 
-
-
-
-
+<div v-else class="">
+  Loading...
+</div>
 
 </template>
